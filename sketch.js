ae79dafcb326f5ec75e130ef8ac15eeaf53bfe32
Wilgnne Khawan <wilgnne.kba@gmail.com>
2020-06-22 21:18:02 -0300

    Backgraund end Hipsta Images

--- sketch.js
+++ sketch.js
@@ -1,7 +1,35 @@
+let imagemCenario;
+let hipstaSpriteSheet;
+
+const spriteSheetW = 220;
+const spriteSheetH = 270;
+const hipstaW = 110;
+const hipstaH = 135;
+
+let hipstaX = 0;
+let hipstaY = 0;
+
+function preload() {
+  imagemCenario = loadImage('imagens/cenario/floresta.png');
+  hipstaSpriteSheet = loadImage('imagens/personagem/correndo.png');
+}
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  hipstaY = height - 140;
 }
 
 function draw() {
-  background(220);
+  background(imagemCenario);
+  image(
+    hipstaSpriteSheet, 
+    hipstaX, 
+    hipstaY,
+    hipstaW, 
+    hipstaH, 
+    0, 
+    0, 
+    spriteSheetW, 
+    spriteSheetH
+  );
 }
