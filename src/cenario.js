5cec86c1106bb9904f9aa58ee03e101a31c8be3b
Wilgnne Khawan <wilgnne.kba@gmail.com>
2020-06-22 21:39:14 -0300

    offset move

--- src/cenario.js
+++ src/cenario.js
@@ -2,22 +2,27 @@ class Cenario {
   constructor(imagem, velocity = 0) {
     this.imagem = imagem;
     this.velocity = velocity;
+    this.offset = 0;
   }
 
   exibe() {
     image(
       this.imagem,
-      -this.velocity,
+      -this.offset,
       0,
       width,
       height
     );
     image(
       this.imagem,
-      width - this.velocity,
+      width - this.offset,
       0,
       width,
       height
     );
+  }
+
+  move() {
+    this.offset = (this.offset + this.velocity) % width;
   }
 }
