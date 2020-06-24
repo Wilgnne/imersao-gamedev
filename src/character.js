b77125523cee264f981618270c6a25ecea8632f1
Wilgnne Khawan <wilgnne.kba@gmail.com>
2020-06-23 21:13:45 -0300

    Generic Character Class

--- src/character.js
+++ src/character.js
@@ -1,18 +1,25 @@
 class Character {
-  constructor(spriteSheet, x = 0, y = 0, w = undefined, h = undefined, spriteSheetInfos = undefined) {
+
+  constructor(spriteSheet, x = 0, y = 0, width = undefined, height = undefined, spriteSheetInfos = undefined) {
     this.spriteSheet = spriteSheet;
     this.spriteSheetW = spriteSheetInfos.spriteSheetW || this.spriteSheet.width;
     this.spriteSheetH = spriteSheetInfos.spriteSheetH || this.spriteSheet.height;
-    this.w = w || this.spriteSheetW;
-    this.h = h || this.spriteSheetH;
+    this.width = width || this.spriteSheetW;
+    this.height = height || this.spriteSheetH;
 
     this.x = x;
     this.y = y;
 
-    this.vertical = spriteSheetInfos.vertical || 1;
-    this.horizontal = spriteSheetInfos.horizontal || 1;
+    // Quantidade de sprites na vertical no spriteSheet
+    this.cols = spriteSheetInfos.cols || 1;
+    // Quantidade de sprites na horizontal no spriteSheet
+    this.rows = spriteSheetInfos.rows || 1;
 
     this.currentSprite = 0;
+  }
+
+  update() {
+    this.draw();
   }
 
   draw() {
@@ -22,8 +29,8 @@ class Character {
       this.spriteSheet,  // SpriteSheel completo
       this.x,            // Coord X do Char
       this.y,            // Coord Y do Char
-      this.w,            // Largura do Char
-      this.h,            // Altura  do Char
+      this.width,        // Largura do Char
+      this.height,       // Altura  do Char
       spriteCoords[0],   // Coord X do inicio da sprite
       spriteCoords[1],   // Coord Y do inicio da sprite
       this.spriteSheetW, // Largura da sprite
@@ -34,8 +41,8 @@ class Character {
 
   getSprite(index) {
     index = Math.floor(index);
-    const x = index % this.horizontal;
-    const y = Math.floor(index / this.vertical);
+    const x = index % this.cols;
+    const y = Math.floor(index / this.cols);
     return [
       this.spriteSheetW * x,
       this.spriteSheetH * y
@@ -43,6 +50,6 @@ class Character {
   }
 
   anim() {
-    this.currentSprite = (this.currentSprite + 1) % 16;
+    this.currentSprite = (this.currentSprite + 1) % (this.rows*this.cols);
   }
 }
