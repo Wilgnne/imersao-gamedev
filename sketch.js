34b590b2454a72ab99d839e7c8b2643223762a82
Wilgnne Khawan <wilgnne.kba@gmail.com>
2020-06-23 21:15:25 -0300

    Aula 2

--- sketch.js
+++ sketch.js
@@ -1,14 +1,19 @@
 let imagemCenario;
 let hipstaSpriteSheet;
+let dropletSpriteSheet;
 let music;
+let jumpSound;
 
 let cenario;
 let hipsta;
+let droplet;
 
 function preload() {
   imagemCenario = loadImage('imagens/cenario/floresta.png');
   hipstaSpriteSheet = loadImage('imagens/personagem/correndo.png');
+  dropletSpriteSheet = loadImage('imagens/inimigos/gotinha.png');
   music = loadSound('sons/trilha_jogo.mp3');
+  jumpSound = loadSound('sons/somPulo.mp3');
 }
 
 function setup() {
@@ -17,7 +22,7 @@ function setup() {
   hipstaY = height - 140;
 
   cenario = new Cenario(imagemCenario, 1);
-  hipsta = new Character(
+  hipsta = new Player(
     hipstaSpriteSheet,
     0,
     height - 140,
@@ -26,18 +31,39 @@ function setup() {
     {
       spriteSheetW: 220,
       spriteSheetH: 270,
-      horizontal: 4,
-      vertical: 4
+      cols: 4,
+      rows: 4
     }
   );
 
+  droplet = new Enemy(
+    dropletSpriteSheet,
+    width + 512,
+    height - 64,
+    64,
+    64,
+    {
+      spriteSheetW: 105,
+      spriteSheetH: 100,
+      cols: 4,
+      rows: 7
+    });
+
   music.loop();
+}
+
+function keyPressed() {
+  if (key === ' ') {
+    hipsta.pula() && jumpSound.play();
+  }
 }
 
 function draw() {
   cenario.exibe();
 
-  hipsta.draw();
+  droplet.update();
+
+  hipsta.update([droplet]);
 
   cenario.move();
 }
